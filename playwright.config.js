31eaf9fd089dc29c78262f44cba3bf4937e71d7b
Vinicius dos Santos <46813491+santosvini@users.noreply.github.com>
2024-11-27 02:07:01 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.saucedemo.com/',
-    headless: false,
+    headless: true,
     viewport: {
       width: 1280,
       height: 720,
